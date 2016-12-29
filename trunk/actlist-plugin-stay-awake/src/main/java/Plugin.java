6ea1020585d36ea7ec9d944640fbd8698b04d407
silentsoft <silentsoft@6448db61-98a1-8449-a9ab-c105f7191d33>
2016-12-29 17:25:36 +0000

    Initial import.

---
+++ trunk/actlist-plugin-stay-awake/src/main/java/Plugin.java
@@ -0,0 +1,49 @@
+import java.awt.MouseInfo;
+import java.awt.Point;
+import java.awt.Robot;
+
+import org.silentsoft.actlist.plugin.ActlistPlugin;
+
+public class Plugin extends ActlistPlugin {
+	
+	private Thread thread;
+	
+	public static void main(String args[]) throws Exception {}
+	
+	public Plugin() {
+		super("Stay Awake");
+	}
+	
+	@Override
+	protected void initialize() throws Exception {
+		setPluginDescription("When Stay Awake is activated, will not gonna be sleep mode.");
+		setPluginVersion("1.0.0");
+		setPluginAuthor("Silentsoft (http://silentsoft.org)");
+	}
+	
+	@Override
+	public void pluginActivated() throws Exception {
+		thread = null;
+		thread = new Thread(() -> {
+			try {
+				Robot robot = new Robot();
+				
+				while (true) {
+					Point mouseLocation = MouseInfo.getPointerInfo().getLocation();
+					robot.mouseMove(mouseLocation.x, mouseLocation.y);
+					
+					Thread.sleep(1000 * 30);
+				}
+			} catch (Exception e) {
+				
+			}
+		});
+		thread.start();
+	}
+	
+	@Override
+	public void pluginDeactivated() throws Exception {
+		thread.interrupt();
+	}
+
+}
